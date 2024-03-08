cfeb88a0d97a66b73d83b32c27afaf13e71d2abd
strdoffin <133888122+fodbrain@users.noreply.github.com>
2024-03-08 22:00:31 +0700

    path update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./kjn.ac.th/**/*.{html,js}"],
   theme: {
     screens: {
       'xl': { 'max': '1279px' },
