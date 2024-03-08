3c3643f445b374ed792e7ec3b6a0fc09099c43fb
strdoffin <133888122+fodbrain@users.noreply.github.com>
2024-03-08 21:30:43 +0700

    first try

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'xl': { 'max': '1279px' },
+      'lg': { 'max': '1023px' },
+      'md': { 'max': '767px' },
+      'sm': { 'max': '639px' },
+  },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
